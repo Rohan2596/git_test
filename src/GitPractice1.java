3c60449b5692b6d68df7cf79e7a548f2f5feb024
Rohan2596 <rohankadam662@gmail.com>
2021-01-26 17:36:08 +0530

    ADDED:Sout statement to main.

---
+++ src/GitPractice1.java
@@ -0,0 +1,10 @@
+
+
+public class GitPractice1 {
+
+    public static void main(String[] args) {
+        System.out.println("Getting Started with Git Tutorial");
+        System.out.println("Configuration for Global Scope and Local Scope.");
+        System.out.println("Saving the git repo and pushing to github");
+    }
+}
